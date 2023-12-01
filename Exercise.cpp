5e4b6113c89f96c838349360fa50c74514e0823e
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2023-12-02 00:02:43 +0600

    first

---
+++ Exercise.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int num1 = 15;
+int num2 = 15;
+
+if(num1==num2){
+    cout<<"They are equal";
+}else{
+    cout<<"they are not equal";
+}
+}
