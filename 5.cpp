6e6f5349e9dd5a7ac600d25832a31ead78dc3469
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2023-12-02 13:41:29 +0600

    updated

---
+++ 5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int age;
+cout<<"Enter Age: ";
+cin>> age;
+
+if(age>=21){
+	cout<<"You are eligible to Vote";
+}else{
+	cout<<"You are not eligible to Vote";
+}
+
+return 0;
+}
---
+++ 6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int num1, num2, num3;
+cout<< "Enter 1st Number: ";
+cin>>num1;
+cout<< "Enter 2nd Number: ";
+cin>>num2;
+cout<< "Enter 3rd Number: ";
+cin>>num3;
+
+if(num1>num2 && num1>num3){
+    cout<<num1<<" is the largest";
+}else if(num2>num3 && num2> num1){
+     cout<<num2 <<" is the largest";
+}else{
+ cout<<num3 <<" is the largest";
+}
+
+
+
+
+
+
+
+return 0;
+}
